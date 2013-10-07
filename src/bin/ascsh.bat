1ad4548b141cb58696ea1cad8951a8ee64f755a0
jeff.ward <jeff.ward@nimbee.com>
2013-10-07 14:13:02 -0600

    Fixed java wrapper script notes

--- src/bin/ascsh.bat
+++ src/bin/ascsh.bat
@@ -1,25 +1,6 @@
 @echo off
 
-rem
-rem  ADOBE CONFIDENTIAL
-rem
-rem  Copyright 2004-2012 Adobe Systems Incorporated
-rem  All Rights Reserved.
-rem
-rem  NOTICE: All information contained herein is, and remains
-rem  the property of Adobe Systems Incorporated and its suppliers,
-rem  if any. The intellectual and technical concepts contained
-rem  herein are proprietary to Adobe Systems Incorporated and its
-rem  suppliers and are protected by trade secret or copyright law.
-rem  Dissemination of this information or reproduction of this material
-rem  is strictly forbidden unless prior written permission is obtained
-rem  from Adobe Systems Incorporated.
-rem
-
-rem
-rem mxmlc.bat script to launch mxmlc-cli.jar in Windows Command Prompt.
-rem On OSX, Unix, or Cygwin, use the mxmlc shell script instead.
-rem
+rem This is a copy of AIRSDK/bin/mxmlc.bat that calls ascsh instead of mxmlc
 
 setlocal
 
